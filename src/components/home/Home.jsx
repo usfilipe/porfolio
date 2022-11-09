5fd1d00f564e95c0689126911a633e0ea56dcffc
Filipe <usfilipe@gmail.com>
2022-11-09 00:18:22 -0500

    Just a test

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <section className="home container" id="home">
       <div className="intro">
         <img src={Me} alt="" className="home__img" />
-        <h1 className="home__name">Filipe Martins</h1>
+        <h1 className="home__name">My Teste</h1>
         <span className="home__education">I'm a Front-End developer</span>
 
         <HeaderSocials />
